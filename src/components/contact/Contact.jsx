068d05f4419a5569e0476215b565e55aa1df0048
Nate <85075459+estin8@users.noreply.github.com>
2021-10-16 09:50:22 -0400

    contact update

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,7 +1,58 @@
 import './contact.scss';
+import { useState } from 'react';
+import { send } from 'emailjs-com';
 
 function Contact() {
-  return <div className="contact" id="contact"></div>;
+  const [toSend, setToSend] = useState({
+    message: '',
+    reply_to: '',
+  });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    send(
+      'service_2xi3ffe',
+      'template_9aywdrq',
+      toSend,
+      'user_IItCBgCoHrX7aw5GlpVex'
+    )
+      .then((response) => {
+        console.log('SUCCESS!', response.status, response.text);
+      })
+      .catch((err) => {
+        console.log('FAILED...', err);
+      });
+  };
+  const handleChange = (e) => {
+    setToSend({ ...toSend, [e.target.name]: e.target.value });
+  };
+  return (
+    <div className="contact" id="contact">
+      <div className="left">
+        <img src="assets/skyline.jpg" alt="cincy skyline" />
+      </div>
+      <div className="right">
+        <h2>Contact</h2>
+        <form onSubmit={onSubmit}>
+          <input
+            type="text"
+            name="reply_to"
+            placeholder="Email"
+            value={toSend.reply_to}
+            onChange={handleChange}
+          />
+          <textarea
+            type="text"
+            name="message"
+            placeholder="Your message"
+            value={toSend.message}
+            onChange={handleChange}
+          />
+          <button type="submit">Send</button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Contact;
