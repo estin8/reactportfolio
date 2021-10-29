a5f54c46c1e625a082fee9e8a47c6de30cf3f382
Nate <85075459+estin8@users.noreply.github.com>
2021-10-29 15:29:24 -0400

    added links to data.js

--- src/data.js
+++ src/data.js
@@ -7,19 +7,20 @@ export const webPortfolio = [
         link: 'https://expressioncheck.w3spaces.com/',
         id: 1
     },
-    {
-        title: "ISS Tracker",
-        description: "An app that maps out the current position of a satetellite based on its NORAD catalog number. The API currently only supports tracking the ISS.",
 
-        img: './assets/satapp.webp',
-        link: '',
-        id: 2
-    },
         {
         title: "Dry County",
         description: "An app experimenting with animation and, in particular, SnapSVG.",
 
         img: './assets/dc.webp',
+        link: 'https://drycounty.glitch.me/',
+        id: 2
+    },
+        {
+        title: "ISS Tracker",
+        description: "An app that maps out the current position of a satetellite based on its NORAD catalog number. The API currently only supports tracking the ISS.",
+
+        img: './assets/satapp.webp',
         link: '',
         id: 3
     },
