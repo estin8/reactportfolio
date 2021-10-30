644973483668259a1c8863d152c36b8732644ebe
Nate <85075459+estin8@users.noreply.github.com>
2021-10-30 11:11:20 -0400

    fixed link order

--- src/data.js
+++ src/data.js
@@ -17,20 +17,20 @@ export const webPortfolio = [
         id: 2
     },
         {
-        title: "ISS Tracker",
-        description: "An app that maps out the current position of a satetellite based on its NORAD catalog number. The API currently only supports tracking the ISS.",
-
-        img: './assets/satapp.webp',
-        link: '',
-        id: 3
-    },
-        {
         title: "Portfolio v1",
         description: "An earlier version of my portfolio that made use of the animating capabilities of GSAP.",
 
         link: 'https://nates.glitch.me/',
         img: './assets/portv1.webp',
         id: 4
+    },
+            {
+        title: "ISS Tracker",
+        description: "An app that maps out the current position of a satetellite based on its NORAD catalog number. The API currently only supports tracking the ISS.",
+
+        img: './assets/satapp.webp',
+        link: '',
+        id: 3
     },
 ]
 
