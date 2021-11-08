5956dca402a8e65bf4a7dc039a2f2607cd5ae1ef
Nate <85075459+estin8@users.noreply.github.com>
2021-11-08 09:21:11 -0500

    updated portfolio/optimized images

--- src/data.js
+++ src/data.js
@@ -1,32 +1,39 @@
 export const webPortfolio = [
+        {
+        title: "Blah Blog - CMS",
+        description: "A blog/content management system utilizing NextJS, GraphQL, and TailwindCSS. ",
+        img: './assets/blog_screenshot.webp',
+        link: 'https://blog-estin8.vercel.app',
+        id: 1
+    },
     {
         title: "Portfolio v1",
         description: "An earlier version of my portfolio that made use of the animating capabilities of GSAP.",
-        img: './assets/portv1.webp',
+        img: './assets/port_screenshot.webp',
         link: 'https://nates.glitch.me/',
-        id: 1
+        id: 2
     },
     {
         title: "Dry County",
         description: "An app experimenting with animation and, in particular, SnapSVG.",
-        img: './assets/dc.webp',
+        img: './assets/dc_screenshot.webp',
         link: 'https://drycounty.glitch.me/',
-        id: 2
+        id: 3
     },
     {
         title: "How are you?",
         description: "An app that utitlizes FaceAPI to measure a persons emotion based on their facial features and returns a response to the user.",
 
-        img: '../assets/faceapi.webp',
+        img: '../assets/hay_screenshot.webp',
         link: 'https://howareyou-phi.vercel.app/',
-        id: 3
+        id: 4
     },
     {
         title: "ISS Tracker",
         description: "An app that maps out the current position of a satetellite based on its NORAD catalog number. The API currently only supports tracking the ISS.",
-        img: './assets/satapp.webp',
+        img: './assets/sat_screenshot.webp',
         link: 'https://satapp.glitch.me/',
-        id: 4
+        id: 5
     },
 ]
 
